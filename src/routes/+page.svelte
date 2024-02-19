b40e0f503d0483bc6d12c32098fe1f0b2f1bbbfb
Sofia Larsson <soflar06@elevad.umea.se>
2024-02-19 12:01:38 +0100

    Klar för start

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,3 +7,40 @@
 
 <h3 >Förnamn Efternamn</h3>
 
+<h3
+
+class="name">Förnamn Efternamn</h3>
+
+<p id="inspo">
+  "The only way to learn a new programming language is by writing
+  programs in it." - Dennis Ritchie, creator of the C programming language.
+  </p>
+  <p style="background-color:pink">
+  "Every great developer you know got there by solving problems they
+  were unqualified to solve until they actually did it." - Patrick McKenzie,
+  software engineer and entrepreneur.
+  </p>
+
+
+<style>
+  h1{
+  color:grey;
+  }
+  h2{
+  color: green;
+  }
+  h3{
+color: #5F9EA0;
+}
+  .name{
+color: rebeccapurple;
+font-family:'Gill Sans'
+}
+#inspo{
+font-family: courier;
+font-size: 160%;
+}
+h1, h2, h3, p {
+display: inline;
+}
+  </style>
