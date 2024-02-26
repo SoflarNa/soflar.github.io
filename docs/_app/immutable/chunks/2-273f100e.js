8ef4e4b3cefa50f0cb7c70c475d900c7707de43d
Sofia Larsson <soflar06@elevad.umea.se>
2024-02-26 11:55:24 +0100

    Klar med foto

--- docs/_app/immutable/chunks/2-273f100e.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/blog/_layout.svelte-da53c871.js";export{t as component};
---
+++ src/routes/photoapp/+page.svelte
@@ -0,0 +1,44 @@
+<script>
+    let image = "https://picsum.photos/id/70/367/267"
+let photo = "https://picsum.photos/id/88/367/267"
+let picture = "https://picsum.photos/id/93/367/267"
+let images = [image,photo,picture,image,photo,picture]
+</script>
+
+
+<title>Photo App</title>
+<h1>Photo App</h1>
+
+
+<div class="photos">
+    {#each images as img}
+         <div class="image-wrapper">
+            <img src={img} alt="flexbox
+            rules">
+        </div>
+    {/each}
+   
+   
+</div>
+
+
+<style>
+.photos{
+display:flex;
+/*Sätter att bredden på vår box enbart får vara 90% av webbfönstrets
+bredd*/
+max-width: 90vw;
+align-items: center;
+justify-content: space-around;
+flex-wrap: wrap;
+}
+.image-wrapper{
+    flex:1;
+    padding: 5px;
+}
+
+.image-wrapper img {
+max-width: 300px;
+}
+
+</style>
